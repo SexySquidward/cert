81d3b44691b619d8b9846cbb8ba9e46452598848
SexySquidward <60376311+SexySquidward@users.noreply.github.com>
2020-11-24 13:16:14 +1030

    Update conf.d

--- nginx-ssl/conf.d
+++ nginx-ssl/conf.d
@@ -2,7 +2,7 @@ server {
      listen [::]:80;
      listen 80;
 
-     server_name haqio.duckdns.org www.haqio.duckdns.org;
+     server_name haqio.duckdns.org;
 
      location ~ /.well-known/acme-challenge {
          allow all; 
